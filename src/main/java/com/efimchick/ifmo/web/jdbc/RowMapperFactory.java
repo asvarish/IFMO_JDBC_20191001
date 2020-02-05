35672ba856cc3fbb7873c62f45c9066f78360cf8
asvarish <42956531+asvarish@users.noreply.github.com>
2020-02-05 19:54:18 +0300

    P3223 Asvarisch Michael Task-2

--- src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
@@ -1,10 +1,31 @@
 package com.efimchick.ifmo.web.jdbc;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+
+import java.math.BigInteger;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class RowMapperFactory {
 
     public RowMapper<Employee> employeeRowMapper() {
-        throw new UnsupportedOperationException();
+        return resultSet -> {
+            try {
+                return new Employee(
+                        new BigInteger(resultSet.getString("ID")),
+                        new FullName(
+                                resultSet.getString("FIRSTNAME"),
+                                resultSet.getString("LASTNAME"),
+                                resultSet.getString("MIDDLENAME")),
+                        Position.valueOf(resultSet.getString("POSITION")),
+                        LocalDate.parse(resultSet.getString("HIREDATE")),
+                        resultSet.getBigDecimal("SALARY")
+                );
+            } catch(SQLException e) {
+                throw null;
+            }
+        };
     }
 }
